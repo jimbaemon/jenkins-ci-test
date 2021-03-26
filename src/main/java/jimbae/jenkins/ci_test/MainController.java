9fc160fad08ac8944e1964f8da91f4a77044c2da
jimbaemon <jimbae@kakao.com>
2021-03-27 00:33:47 +0900

    feat : CD by profiles

--- src/main/java/jimbae/jenkins/ci_test/MainController.java
+++ src/main/java/jimbae/jenkins/ci_test/MainController.java
@@ -1,6 +1,10 @@
 package jimbae.jenkins.ci_test;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +15,24 @@ public class MainController {
 	@Value("${app.version}")
 	private String version;
 
+	@Autowired
+	private Environment env;
+
 	@GetMapping("/")
 	public ResponseEntity main(){
 
 		return ResponseEntity.ok(version);
+
+	}
+
+	@GetMapping("/profile")
+	public ResponseEntity profile(){
+
+		return ResponseEntity.ok(
+			Arrays.stream(env.getActiveProfiles())
+				.findFirst()
+				.orElse("")
+		);
 
 	}
 
